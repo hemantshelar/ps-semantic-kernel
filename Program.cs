9ff0754bb4c68eb80ecfe9351975ae4bc89c794e
Hemant <hemant.shelar@gmail.com>
2025-06-17 07:23:53 +1000

    Initial project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ps_semantic_kernel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
