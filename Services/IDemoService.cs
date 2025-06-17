7c48c00f21e7d7815cd4226d6feb5da719fa7d06
Hemant <hemant.shelar@gmail.com>
2025-06-17 11:03:23 +1000

    Basic di working

---
+++ Services/IDemoService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using ps_semantic_kernel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps_semantic_kernel.Services
+{
+    public interface IDemoService
+    {
+        void RunDemo();
+    }
+
+    public class DemoService : IDemoService
+    {
+        private readonly OpenAIConfig _config;
+
+        public DemoService(IOptions<OpenAIConfig> config)
+        {
+            _config = config.Value;
+        }
+        public void RunDemo()
+        {
+            Console.WriteLine("Demo Service is running...");
+            // Add your demo logic here
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+| API      | Column 2 | Column 3 | Column 4 | Column 5 |
+|----------|----------|----------|----------|----------|
+| [OpenAI](https://openai.com/api/pricing/)  | [Pricing](https://openai.com/api/pricing/)  | Value 3  | Value 4  | Value 5  |
+| Value 6  | Value 7  | Value 8  | Value 9  | Value 10 |
+| Value 11 | Value 12 | Value 13 | Value 14 | Value 15 |
